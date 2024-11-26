199e0eafdc4bba20c9a5bb4a84a720b0800d30c6
tbixler17 <tbixler1@terpmail.umd.edu>
2024-11-26 15:50:49 -0500

    Update weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,6 +1,6 @@
 """
 INST 326 Weather Forecast Final Project
-Group Members: Weston Marhefka,  Tiffany Bixler, Alex Lopez
+Group Members: Weston Marhefka
 """
 
 import requests
@@ -20,8 +20,21 @@ def get_weather_data(city_name, api_key):
     city_data = geocode_response.json()[0]
     lat, lon = city_data['lat'], city_data['lon']
     
-    #Add code to get the weather forecast for the coordinates
-   
+    # Get the weather forecast for the coordinates
+    weather_url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={api_key}"
+    weather_response = requests.get(weather_url)
+    
+    if weather_response.status_code != 200:
+        print("Couldn't retrieve weather data. Please try again later.")
+        return None, None, None
+    
+    weather_data = weather_response.json()
+    
+  
+    timezone_str = TimezoneFinder().timezone_at(lat=lat, lng=lon)
+    timezone = pytz.timezone(timezone_str) if timezone_str else pytz.UTC
+    
+    return weather_data, timezone, city_data['name']
 
 def process_forecast(forecast_data, timezone):
     daily_forecast = {}
@@ -33,10 +46,20 @@ def process_forecast(forecast_data, timezone):
         if date_str not in daily_forecast:
             daily_forecast[date_str] = []
         
-        # Add code for weather details for each time slot
+        # Adds weather details for each time slot
+        forecast_info = {
+            'time': local_time.strftime('%I:%M %p'),
+            'description': entry['weather'][0]['description'].title(),
+            'temp': entry['main']['temp'],
+            'temp_min': entry['main']['temp_min'],
+            'temp_max': entry['main']['temp_max'],
+            'humidity': entry['main']['humidity'],
+            'wind_speed': entry['wind']['speed']
+        }
         
+        daily_forecast[date_str].append(forecast_info)
     
-    return 
+    return daily_forecast
 
 def display_forecast(daily_forecast, city_name):
     # Show the weather forecast
@@ -50,7 +73,7 @@ def display_forecast(daily_forecast, city_name):
 def main():
     api_key = '37eff77313390fb273b82a494b012c65'
     city_name = input("Enter city name: ").strip()
-    #add if statements for 
+    
     if not city_name:
         print("City name is required.")
         return
@@ -66,3 +89,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
